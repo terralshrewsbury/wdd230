63747b48e8838c499552c3bbe265141a22781b25
Turrtle0612 <123220471+Turrtle0612@users.noreply.github.com>
2024-03-28 08:11:11 -0600

    added media query to my homepage Scoots

---
+++ scoots/scripts/hamburgermenu.js
@@ -0,0 +1,8 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	console.log('Hamburger menu button clicked');
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
